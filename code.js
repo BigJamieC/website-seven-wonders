9d50318500b020af2bdbb7064db291e8a6d56c12
BigJamieC <164178697+BigJamieC@users.noreply.github.com>
2024-12-16 14:48:03 +0000

    Added code that gets data from URL for the form for a personalised response

--- code.js
+++ code.js
@@ -1,4 +1,12 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault()
-    document.querySelector('#confirmation').innerHTML = "Thank you for partaking, your response has been submitted"
-});
+// document.querySelector('form').addEventListener('submit', function(e) {
+//     e.preventDefault()
+//     document.querySelector('#confirmation').innerHTML = "Thank you for partaking, your response has been submitted"
+// });
+
+var confirmationElement = document.querySelector('#confirmation')
+
+var urlData = location.href.split("?")[1].split("&")
+var firstName = urlData[0].split("=")[1]
+var lastName = urlData[1].split("=")[1]
+
+confirmationElement.innerHTML = `Thank you for partaking ${firstName} ${lastName}, your response has been submitted`
